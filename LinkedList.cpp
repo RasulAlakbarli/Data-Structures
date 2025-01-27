05d72f51b4b4ead04ef7d329d51db211c5eff213
RasulAlakbarli <alakbarlirasul@gmail.com>
2025-01-27 11:03:42 +0100

    Added: Linked list, Readme files

---
+++ LinkedList.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+
+// Linked list 
+struct Node{
+    int data;
+    Node* next;
+};
+
+class LinkedList{
+    Node* head = nullptr;
+    Node* tail = nullptr;
+
+    public:
+        // Method to insert an element to the beginning of the list
+        void insertAtBeginning(int num){
+            Node* newNode = new Node;
+            newNode->data = num;
+            newNode->next = head;
+
+            head = newNode;
+            
+            if (tail == nullptr){
+                tail = head;
+            }
+        }
+
+        // Method to insert an element to the end of the list
+        void insertAtEnd(int num){
+            if (head == nullptr){
+                head = new Node;
+                head->data = num;
+                head->next = nullptr;
+
+                tail = head;
+            }
+            else{
+                Node* newNode = new Node;
+                newNode->data = num;
+                newNode->next = nullptr;
+
+                tail->next = newNode;
+                tail = newNode;
+            }
+        }
+
+        // Method to delete an element in the beginning of the list
+        void deleteAtBeginning(){
+            
+        }
+
+        // Method to delete an element in the end of the list
+        void deleteAtEnd(){
+
+        }
+
+        // Method to print all elements from start to end
+        void traverse(){
+            if (head == nullptr){
+                std::cout << "List is empty" << std::endl;
+            }
+            else{
+                Node* currentNode = head;
+                while (currentNode != nullptr){
+                    std::cout << currentNode->data << std::endl;
+                    currentNode = currentNode->next;
+                }
+            }
+        }
+
+        // Method to delete all nodes
+        ~LinkedList(){
+            Node* currentNode = head;
+            while (currentNode != nullptr){
+                Node* nextNode = currentNode->next;
+                delete currentNode;
+                currentNode = nextNode;
+            }
+        }
+};
+
+
+int main() {
+    LinkedList list;
+
+    // Insert at beginning
+    list.insertAtBeginning(10);
+    list.insertAtBeginning(5);
+
+    // Insert at end
+    list.insertAtEnd(15);
+    list.insertAtEnd(20);
+
+    // Insert at beginning
+    list.insertAtBeginning(3);
+    list.insertAtBeginning(1);
+
+    // Traverse and print
+    std::cout << "Linked list elements:" << std::endl;
+    list.traverse();
+
+    // Destructor is invoked automatically when the program ends.
+    return 0;
+}
+
---
+++ README.md
@@ -0,0 +1,37 @@
+# **Data Structures in C++**
+
+This repository contains C++ implementations of various data structures. This repo is created to help beginners like me understand the basics of data structures and algorithms and learn c++ programming.
+
+---
+
+## **Implemented Data Structures**
+
+1. **Singly Linked List**
+   - **Operations**:
+     - Insertion at the beginning
+     - Insertion at the end
+     - Deletion at the beginning
+     - Deletion at the end
+     - Traversal
+   - **Key Features**:
+     - Efficient insertion using `head` and `tail` pointers.
+     - Memory safety with a destructor to avoid memory leaks.
+   - [Source Code](./LinkedList.cpp) 
+
+---
+
+## **Planned Implementations**
+- Doubly Linked List
+- Stack (using arrays and linked lists)
+- Queue (simple and circular)
+- Binary Search Tree (BST)
+- Hash Table
+- Graph (Adjacency List and Matrix)
+
+---
+
+## **How to Use**
+
+1. Clone the repository:
+   ```bash
+   git clone git@github.com:RasulAlakbarli/Data-Structures.git
