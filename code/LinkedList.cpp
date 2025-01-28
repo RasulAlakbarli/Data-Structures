53fbdcb73fd2e3f35a178daef441242da7574ab2
RasulAlakbarli <alakbarlirasul@gmail.com>
2025-01-28 11:06:03 +0100

    Added: deleteAtBeginning and deleteAtEnd functions

--- code/LinkedList.cpp
+++ code/LinkedList.cpp
@@ -44,12 +44,37 @@ class LinkedList{
 
         // Method to delete an element in the beginning of the list
         void deleteAtBeginning(){
-            
+            if (head == nullptr){
+                return;
+            }
+            Node* next = head->next;
+            delete head;
+            head = next;
+            if (head == nullptr){
+                tail = nullptr;
+            }
         }
 
         // Method to delete an element in the end of the list
         void deleteAtEnd(){
-
+            if (head == nullptr){
+                return;
+            }
+            if (head == tail) {
+                delete head;
+                head = nullptr;
+                tail = nullptr;
+                return;
+            }
+            
+            Node* currentNode = head;
+            while (currentNode->next != tail){
+                currentNode = currentNode->next;
+            }
+            delete tail;
+            tail = currentNode;
+            tail->next = nullptr;
+            return;
         }
 
         // Method to print all elements from start to end
@@ -92,7 +117,15 @@ int main() {
     // Insert at beginning
     list.insertAtBeginning(3);
     list.insertAtBeginning(1);
+    
+    // Delete at the beginning
+    list.deleteAtBeginning();
+    list.insertAtBeginning(0);
 
+    // Delete at the end
+    list.deleteAtEnd();
+    list.insertAtEnd(18);
+    
     // Traverse and print
     std::cout << "Linked list elements:" << std::endl;
     list.traverse();
